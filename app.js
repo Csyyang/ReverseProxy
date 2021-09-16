f92190850a4f3243626337362a565a49a1150508
csy23333 <693765678@qq.com>
2021-09-16 15:56:16 +0800

    添加证书

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
 const { setConfig } = require('./util.js')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
+// 代理配置
 const photoOption = setConfig('http://localhost:8080', {
     '^/photo': ''
 })
-
 const phtotProxy = createProxyMiddleware(photoOption);
 app.use('/photo', phtotProxy);
-app.listen(80)
+
+// https 配置
+const HTTPS_OPTOIN = {
+    key: fs.readFileSync(path.resolve(__dirname, './https/2_yangyangcsy.cn.key'), 'utf8'),
+    cert: fs.readFileSync(path.resolve(__dirname,'./https/1_yangyangcsy.cn_bundle.crt'), 'utf8')
+  };
+
+https.createServer(HTTPS_OPTOIN, app).listen(443);
+
+// app.listen(80)
